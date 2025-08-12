69b19b15534d4fd217f3186362beacd6d37d3110
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-08-12 16:42:15 -0300

    seção:Support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
+import { SupportSection } from "@/components/support-section";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <article className="flex flex-col">
       <HeroSection/>
       <FeatureSection/>
+      <SupportSection/>
     </article>
     </>
   );
